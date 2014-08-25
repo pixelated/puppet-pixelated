6cb83b7a000f6069189218c5f5e047ea1438ef8c
Alexandre Pretto Nunes <anunes@thoughtworks.com>
2014-08-25 16:03:33 -0300

    Add License file and license+copyright notice on bootstrap script

--- buildscripts/bootstrap.sh
+++ buildscripts/bootstrap.sh
@@ -1,3 +1,18 @@
+#
+# Copyright (c) 2014 ThoughtWorks Deutschland GmbH
+#
+# Pixelated is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pixelated is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Pixelated. If not, see <http://www.gnu.org/licenses/>.
 echo "deb http://debian.mirror.iphh.net/debian wheezy-backports main" > /etc/apt/sources.list.d/backports.list
 apt-get update
 apt-get -y install puppet rsync ruby-hiera-puppet git ruby1.9.1-dev rake jq

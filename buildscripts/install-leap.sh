095880b077bf7c4ae6a8a7c293779eb9dee66bf5
Christoph Kluenter <ckluente@thoughtworks.com>
2014-09-17 11:58:05 +0200

    merge jobs for installing leap

--- buildscripts/install-leap.sh
+++ buildscripts/install-leap.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 echo "==============================================="
+echo "preparing leap"
+echo "==============================================="
+
+echo "deb http://debian.mirror.iphh.net/debian wheezy-backports main" > /etc/apt/sources.list.d/backports.list
+apt-get update
+apt-get -y install puppet rsync ruby-hiera-puppet git ruby1.9.1-dev rake jq
+apt-get clean
+
+echo "==============================================="
 echo "installing leap"
 echo "==============================================="
 #gem install leap_cli
@@ -15,3 +24,4 @@ git clone -b develop https://leap.se/git/leap_platform.git
 cd /home/leap/leap_platform
 git submodule sync
 git submodule update --init
+
--- buildscripts/prepare-baseimage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "deb http://debian.mirror.iphh.net/debian wheezy-backports main" > /etc/apt/sources.list.d/backports.list
-apt-get update
-apt-get -y install puppet rsync ruby-hiera-puppet git ruby1.9.1-dev rake jq
-apt-get clean
-

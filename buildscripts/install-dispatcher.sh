8529b877fa2f63315726789a6a1fcb8697b1cb8b
Christoph Kluenter <ckluente@thoughtworks.com>
2014-10-09 17:31:19 +0200

    run apt-get noninteractive

--- buildscripts/install-dispatcher.sh
+++ buildscripts/install-dispatcher.sh
@@ -6,5 +6,5 @@ echo "deb http://packages.pixelated-project.org/debian wheezy-snapshots main" > 
 echo "deb http://packages.pixelated-project.org/debian wheezy main"          >> /etc/apt/sources.list.d/pixelated.list
 apt-key adv --keyserver pool.sks-keyservers.net --recv-key 287A1542472DC0E3 # install the pixelated apt key
 apt-get update
-apt-get -t wheezy-backports -y install pixelated-dispatcher linux-image-amd64 lxc-docker
+DEBIAN_FRONTEND=noninteractive apt-get -t wheezy-backports -y install pixelated-dispatcher linux-image-amd64 lxc-docker
 
--- buildscripts/install-leap.sh
+++ buildscripts/install-leap.sh
@@ -6,7 +6,7 @@ echo "==============================================="
 
 echo "deb http://debian.mirror.iphh.net/debian wheezy-backports main" > /etc/apt/sources.list.d/backports.list
 apt-get update
-apt-get -y install puppet rsync ruby-hiera-puppet git ruby1.9.1-dev rake jq
+DEBIAN_FRONTEND=noninteractive apt-get -y install puppet rsync ruby-hiera-puppet git ruby1.9.1-dev rake jq
 apt-get clean
 
 echo "==============================================="

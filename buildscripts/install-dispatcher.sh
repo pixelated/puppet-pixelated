1079ddeb732051cb0d03c05ba34b851a4bc45f74
Christoph Kluenter <ckluente@thoughtworks.com>
2014-11-13 10:06:23 +0100

    pin SSL Fingerprint on proxy

--- buildscripts/install-dispatcher.sh
+++ buildscripts/install-dispatcher.sh
@@ -13,6 +13,13 @@ apt-key adv --keyserver pool.sks-keyservers.net --recv-key 287A1542472DC0E3 # in
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -t wheezy-backports -y install pixelated-dispatcher linux-image-amd64 lxc-docker
 
+
+echo "==============================================="
+echo "Pinning SSL Fingerprint"
+echo "==============================================="
+FINGERPRINT=$(openssl x509 -in /etc/ssl/certs/ssl-cert-snakeoil.pem -noout -fingerprint -sha1 | cut -d'=' -f 2
+echo "PIXELATED_MANAGER_FINGERPRINT=$FINGERPRINT" >> /etc/default/pixelated-dispatcher-proxy
+
 echo "==============================================="
 echo "cleaning up"
 echo "==============================================="

af8d6bb2420f7ae408018bbe0d87f65ef96d1ca2
varac <varacanero@zeromail.org>
2014-12-02 11:02:48 +0100

    fixed typo, require keyfile before adding it to apt keys

--- modules/pixelated/manifests/apt.pp
+++ modules/pixelated/manifests/apt.pp
@@ -5,7 +5,7 @@ class pixelated::apt {
   apt::sources_list { 'pixelated.list':
     content => "deb http://packages.pixelated-project.org/debian wheezy-snapshots main\ndeb http://packages.pixelated-project.org/debian wheezy main\n",
     require => Exec[add_pixelated_key],
-    notify  =>  Exec[refresh_apt],
+    notify  => Exec[refresh_apt],
   }
 
   file { '/home/leap/0x287A1542472DC0E3_packages@pixelated-project.org.asc':
@@ -13,8 +13,9 @@ class pixelated::apt {
     notify => Exec['add_pixelated_key']
   }
   exec{'add_pixelated_key':
-    command     => '/usr/bin/apt-ke add /home/leap/0x287A1542472DC0E3_packages@pixelated-project.org.asc',
+    command     => '/usr/bin/apt-key add /home/leap/0x287A1542472DC0E3_packages@pixelated-project.org.asc',
     refreshonly => true,
+    require     => File['/home/leap/0x287A1542472DC0E3_packages@pixelated-project.org.asc'],
   }
 }
 

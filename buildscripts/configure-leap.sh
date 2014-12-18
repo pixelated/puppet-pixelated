c55f2eeee308adda948ec74d839669516332aa55
Christoph Kluenter <ckluente@thoughtworks.com>
2014-12-18 11:32:40 +0100

    create directory for logging
    we tell leap_cli to log to /var/log/leap but never create
    that directory

--- buildscripts/configure-leap.sh
+++ buildscripts/configure-leap.sh
@@ -13,6 +13,7 @@ mkdir /home/leap/configuration
 cd /home/leap/configuration
 leap $OPTS new --contacts no-reply@try.pixelated-project.org --domain try.pixelated-project.org --name LEAP_Example --platform=/home/leap/leap_platform .
 echo -e '\n@log = "/var/log/leap/deploy.log"' >> Leapfile
+mkdir -p /var/log/leap
 ssh-keygen -f /root/.ssh/id_rsa -P ""
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 mkdir -p /home/leap/configuration/files/nodes/pixelated

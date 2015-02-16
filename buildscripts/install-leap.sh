450a6fc974db243ebdb7c97aa84f864da18734dc
Christoph Kluenter <ckluente@thoughtworks.com>
2015-02-16 14:12:00 +0100

    update leap package key before installing leap
    the leap key expires sometimes. So we need to refetch it to
    prevent a failing apt-get

--- buildscripts/install-leap.sh
+++ buildscripts/install-leap.sh
@@ -15,6 +15,10 @@ apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y install puppet rsync ruby-hiera-puppet git ruby1.9.1-dev rake jq
 apt-get clean
 
+# update leap package key
+apt-key adv --keyserver pool.sks-keyservers.net --recv-keys 1E34A1828E207901
+
+
 echo "==============================================="
 echo "installing leap"
 echo "==============================================="

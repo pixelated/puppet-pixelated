0f718c0c402b798d029efa76f27b7b6f824768e1
Christoph Kluenter <ckluente@thoughtworks.com>
2015-02-17 10:20:20 +0100

    install leap from git.
    again, the gem does not work with the platform version:
     + leap --no-color add-user --self
     = This leap command (v1.6.2) is not compatible with the platform /home/leap/leap_platform (v0.6.1). You need leap command 1.6.3 to 1.99.
     = bailing out
    
    Lets see how long this will last

--- buildscripts/install-leap.sh
+++ buildscripts/install-leap.sh
@@ -25,12 +25,12 @@ echo "==============================================="
 mkdir /home/leap
 
 # use released leap_cli gem instead of building from source
-gem install leap_cli
+# gem install leap_cli
 
-#git clone -b develop https://leap.se/git/leap_cli.git /home/leap/leap_cli
-#cd /home/leap/leap_cli
-#rake build
-#rake install
+git clone -b develop https://leap.se/git/leap_cli.git /home/leap/leap_cli
+cd /home/leap/leap_cli
+rake build
+rake install
 
 git clone --recursive https://github.com/pixelated-project/leap_platform.git /home/leap/leap_platform
 

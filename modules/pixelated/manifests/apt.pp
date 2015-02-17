8469be8ac9eeba6664d18fa159728e506374e8af
Christoph Kluenter <ckluente@thoughtworks.com>
2015-02-17 13:09:55 +0100

    install some pytyhon dependencies from backports

--- modules/pixelated/manifests/apt.pp
+++ modules/pixelated/manifests/apt.pp
@@ -2,6 +2,10 @@
 
 class pixelated::apt {
 
+  apt::preferences_snippet { ['python-requests','python-six','python-tornado','linux-image-amd64']:
+    release  => "${::lsbdistcodename}-backports",
+    priority => 999
+  }
   apt::sources_list { 'pixelated.list':
     content => "deb http://packages.pixelated-project.org/debian wheezy-snapshots main\ndeb http://packages.pixelated-project.org/debian wheezy main\n",
     require => Exec[add_pixelated_key],

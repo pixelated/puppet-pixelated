76915c1b2d2bafc5e923cb0cdffe214961550062
Christoph Kluenter <ckluente@thoughtworks.com>
2015-06-03 14:47:57 +0200

    Revert "remove backports pinning of linux-image-amd64 (#89)"
    This reverts commit 2f0c53484ee13ec2bb64e717ba67343bb2923e82.
    
    We actually need to use a newer kernel for docker

--- modules/pixelated/manifests/apt.pp
+++ modules/pixelated/manifests/apt.pp
@@ -2,7 +2,7 @@
 
 class pixelated::apt {
 
-  apt::preferences_snippet { ['python-urllib3', 'python-requests','python-six']:
+  apt::preferences_snippet { ['python-urllib3', 'python-requests','python-six','linux-image-amd64']:
     release  => "${::lsbdistcodename}-backports",
     priority => 999
   }

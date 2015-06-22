f78519f6ee342e31ae3476e391be84e820e095c8
varac <varacanero@zeromail.org>
2015-06-22 16:48:04 +0200

    use httpredir.debian.org as deb mirror as iphh mirror is down

--- buildscripts/install-leap.sh
+++ buildscripts/install-leap.sh
@@ -10,7 +10,7 @@ echo "==============================================="
 echo "preparing leap"
 echo "==============================================="
 
-echo "deb http://debian.mirror.iphh.net/debian wheezy-backports main" > /etc/apt/sources.list.d/backports.list
+echo "deb http://httpredir.debian.org/debian/ wheezy-backports main" > /etc/apt/sources.list.d/backports.list
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y install puppet rsync ruby-hiera-puppet git ruby1.9.1-dev rake jq
 apt-get clean

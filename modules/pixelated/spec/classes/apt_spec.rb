9a51b1ad6bebd3766037c5ddd6586f2bcd7b27f6
Christoph Kluenter <ckluente@thoughtworks.com>
2015-07-14 15:29:50 +0200

    add tests

---
+++ modules/pixelated/spec/classes/apt_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'pixelated::apt' do
+  let(:facts) do
+      {
+        :operatingsystem  => 'Debian',
+        :lsbdistcodename  => 'wheezy',
+      }
+  end
+  it { should contain_file('/etc/apt/preferences.d/python-tornado').with_content(/Pin: release o=pixelated/) }
+  it { should contain_file('/etc/apt/preferences.d/python-tornado').with_content(/Package: python-tornado/) }
+  it { should contain_file('/etc/apt/preferences.d/python-tornado').with_content(/Pin-Priority: 999/) }
+  it { should contain_file('/etc/apt/sources.list.d/pixelated.list') }
+
+  %w( python-urllib3 python-requests python-six linux-image-amd64).each do | package |
+    it { should contain_file("/etc/apt/preferences.d/#{package}").with_content(/wheezy/) }
+  end
+
+
+end
---
+++ modules/pixelated/spec/classes/dispatcher_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'pixelated::dispatcher' do
+  let(:facts) do
+      {
+        :operatingsystem  => 'Debian',
+        :osfamily         => 'Debian',
+        :lsbdistcodename  => 'wheezy',
+      }
+  end
+
+  it { should contain_class('pixelated::syslog') }
+  # testing if shorewall::masq generates the files
+  it { should contain_concat__fragment('masq-100-docker_masq').with_content(/eth0 172\.17\.0\.0\/16/)}
+  it { should contain_concat__fragment('zones-100-dkr').with_content(/dkr ipv4/)}
+  it { should contain_concat__fragment('policy-1-dkr-to-all').with_content(/dkr all ACCEPT/)}
+  it { should contain_concat__fragment('rules-200-net2fw-pixelated-dispatcher').with_content(/pixelated_dispatcher\(ACCEPT\) net \$FW/)}
+  it { should contain_concat__fragment('rules-201-dkr2fw-https').with_content(/HTTPS\(ACCEPT\) dkr \$FW/)}
+  it { should contain_concat__fragment('rules-202-dkr2fw-leap-api').with_content(/leap_webapp_api\(ACCEPT\) dkr \$FW/)}
+  it { should contain_concat__fragment('rules-203-dkr2fw-leap-mx').with_content(/leap_mx\(ACCEPT\) dkr \$FW/)}
+end

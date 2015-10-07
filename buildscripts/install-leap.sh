e81dba9dc1f02c6067343156b0b6538119d2524f
Christoph Kluenter <ckluente@thoughtworks.com>
2015-10-07 22:11:39 +0200

    this script is not run as root anymore
    since the script configure-leap.sh can't run as root,
    this script shouldn't run as root either. to avoid  permission problems.
    we use sudo inside the script now if we need root privileges

--- buildscripts/install-leap.sh
+++ buildscripts/install-leap.sh
@@ -10,35 +10,33 @@ echo "==============================================="
 echo "preparing leap"
 echo "==============================================="
 
-echo "deb http://debian.mirror.iphh.net/debian wheezy-backports main" > /etc/apt/sources.list.d/backports.list
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get -y install puppet rsync ruby-hiera-puppet git ruby1.9.1-dev rake jq
-apt-get clean
+sudo sh -c 'echo "deb http://debian.mirror.iphh.net/debian wheezy-backports main" > /etc/apt/sources.list.d/backports.list'
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install puppet rsync ruby-hiera-puppet git ruby1.9.1-dev rake jq
+sudo apt-get clean
 
 # update leap package key
-apt-key adv --keyserver pool.sks-keyservers.net --recv-keys 1E34A1828E207901
+sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-keys 1E34A1828E207901
 
 
 echo "==============================================="
 echo "installing leap"
 echo "==============================================="
-mkdir /home/leap
-
-# use released leap_cli gem instead of building from source
-# only works for leap_platform:master
-#gem install leap_cli
+ME=$USER
+sudo mkdir /home/leap
+sudo chown $ME /home/leap
 
 # build leap_cli from source so it works together with leap_platform:develop
 git clone -b develop https://leap.se/git/leap_cli.git /home/leap/leap_cli
 cd /home/leap/leap_cli
 rake build
-rake install
+sudo rake install
 
 git clone --recursive --branch develop https://github.com/pixelated/leap_platform.git /home/leap/leap_platform
 
 echo "==============================================="
 echo "cleaning up"
 echo "==============================================="
-apt-get -f install
-sync
+sudo apt-get -f install
+sudo sync
 sleep 10

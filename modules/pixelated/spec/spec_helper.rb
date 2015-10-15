f87e4724cafda6397648feb4f0aa37ade5366b65
Christoph Kluenter <ckluente@thoughtworks.com>
2015-10-15 13:55:04 +0200

    enable hiera in our tests

--- modules/pixelated/spec/spec_helper.rb
+++ modules/pixelated/spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'rspec-puppet'
+require 'rspec-puppet-utils'
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'puppetlabs_spec_helper/puppetlabs_spec_helper'
 
@@ -7,4 +8,5 @@ fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
 end

1d9c58006dcbecc5f1672c9c4e9f860328175700
varac <varacanero@zeromail.org>
2015-11-16 11:13:01 +0100

    fix test for last commit

--- spec/classes/docker_spec.rb
+++ spec/classes/docker_spec.rb
@@ -13,5 +13,5 @@ describe 'pixelated::docker' do
   it { should contain_file('/etc/init.d/docker')}
   it { should contain_exec('configure_docker').with_refreshonly('true')}
   it { should contain_file('/usr/local/bin/renew-docker-images.sh')}
-  it { should contain_cron('renew-docker').with_command("/usr/local/bin/renew-docker-images.sh")}
+  it { should contain_cron('renew-docker').with_command("/usr/local/bin/renew-docker-images.sh 1>&2 >> /var/log/pixelated/docker-renew.log")}
 end

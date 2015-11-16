f57901b38c7fc841b09a5d52f89c4abb7f8fcf25
Christoph <chris@inferno.nadir.org>
2015-11-16 13:27:22 +0100

    install leap-mx from our repo

--- manifests/apt.pp
+++ manifests/apt.pp
@@ -4,7 +4,7 @@ class pixelated::apt {
 
   include apt
   include lsb
-  apt::preferences_snippet { ['python-urllib3', 'python-requests','python-six','linux-image-amd64']:
+  apt::preferences_snippet { ['python-urllib3', 'python-requests','python-six','linux-image-amd64','leap-mx']:
     release  => "${::lsbdistcodename}-backports",
     priority => 999
   }

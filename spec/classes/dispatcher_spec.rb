38c07fd490c57c61b09d8697c6e22aeca1922d34
Christoph <chris@inferno.nadir.org>
2015-11-18 16:45:37 +0100

    requirements changed. adopt test accordingly

--- spec/classes/dispatcher_spec.rb
+++ spec/classes/dispatcher_spec.rb
@@ -29,13 +29,9 @@ require 'spec_helper'
 
     it { should contain_apache__vhost__file('dispatcher').with_content(/mail.example.com/)}
 
-    it "should require leap webapp" do
-      should contain_file('/srv/leap/webapp/config/customization/locales/en.yml').with(
-        'require' => 'Vcsrepo[/srv/leap/webapp]',
-      )
-      should contain_file('/srv/leap/webapp/config/customization/views/common/_download_button.html.haml').with(
-        'require' => 'Vcsrepo[/srv/leap/webapp]',
-      )
+    it "should configure leap webapp" do
+      should contain_file('/srv/leap/webapp/config/customization/locales/en.yml').with( 'require' => 'File[/srv/leap/webapp/config/customization/views/common]',)
+      should contain_file('/srv/leap/webapp/config/customization/views/common/_download_button.html.haml')
       should contain_file('/srv/leap/webapp/config/customization/views/users/show.html.haml').with_content(/mail.example.com/)
     end
   end

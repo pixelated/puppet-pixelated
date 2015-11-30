c0c6508fa4f07aa033c9ca91431a9cb43b19765c
Christoph <chris@inferno.nadir.org>
2015-11-30 14:31:18 +0100

    checks should be comliant with check_mk

--- files/check_mk/check_dispatcher_manager.sh
+++ files/check_mk/check_dispatcher_manager.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-/usr/lib/nagios/plugins/check_procs -a '/usr/bin/pixelated-dispatcher manager' -c 1:1
-exit $?
+STATUS[0]='OK'
+STATUS[1]='Critical'
+STATUS[2]='Critical'
+CHECKNAME='Dispatcher_Proxy'
+
+MESSAGE=$(/usr/lib/nagios/plugins/check_procs -a '/usr/bin/pixelated-dispatcher manager' -c 1:1)
+SERVICESTATUS=$?
+echo "${SERVICESTATUS} ${CHECKNAME} ${MESSAGE}"
+

947a48c400259b2b64dc5e21238354d02efb8c58
Christoph <chris@inferno.nadir.org>
2015-12-17 23:04:27 +0100

    install docker from docker.com

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -2,14 +2,19 @@
 class pixelated::docker {
   $services    = hiera('services')
   file{'/etc/init.d/docker':
-    source => 'puppet:///modules/pixelated/docker.init',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
+    source  => 'puppet:///modules/pixelated/docker.init',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Package['docker-engine'],
   }
   service{'docker':
     ensure    => running,
     hasstatus => true,
+    require   => Package['docker-engine'],
+  }
+  package{'docker-engine':
+    ensure  => installed,
   }
   exec{'configure_docker':
     command     => "/bin/sed -E -i 's/^.\\?DOCKER_OPTS.*/DOCKER_OPTS=--iptables=false/' /etc/default/docker",
--- spec/classes/docker_spec.rb
+++ spec/classes/docker_spec.rb
@@ -10,10 +10,12 @@ describe 'pixelated::docker' do
         :testscenario     => 'single_node',
       }
   end
-  it { should contain_service('docker')}
-  it { should contain_file('/etc/init.d/docker')}
+  it { should contain_service('docker').that_requires('Package[docker-engine]')}
+  it { should contain_file('/etc/init.d/docker').that_requires('Package[docker-engine]')}
   it { should contain_exec('configure_docker').with_refreshonly('true')}
+  it { should contain_exec('configure_docker')}
   it { should contain_file('/usr/local/bin/renew-docker-images.sh')}
+  it { should contain_package('docker-engine')}
   it { should contain_cron('renew-docker').with_command("/usr/local/bin/renew-docker-images.sh 1>&2 >> /var/log/pixelated/docker-renew.log")}
   it { should contain_concat__fragment('zones-100-dkr').with_content(/dkr ipv4/)}
   it { should contain_concat__fragment('policy-1-dkr-to-all').with_content(/dkr all ACCEPT/)}

7eeede980033df66c0538771c1c3de379dadb3ea
varac <varacanero@zeromail.org>
2015-12-28 11:38:19 +0100

    only install backports kernel on wheezy

--- manifests/apt/preferences.pp
+++ manifests/apt/preferences.pp
@@ -1,17 +1,29 @@
 # pin packages so they have precedence over those from the leap repo
 class pixelated::apt::preferences {
 
-  apt::preferences_snippet { ['python-urllib3', 'python-requests','python-six','linux-image-amd64']:
+  apt::preferences_snippet { ['python-urllib3', 'python-requests','python-six']:
     release  => "${::lsbdistcodename}-backports",
     priority => 999
   }
 
-  # install python-docker from backports on jessie
-  if $::lsbdistcodename == 'jessie' {
-    apt::preferences_snippet { ['python-docker']:
-      release  => "${::lsbdistcodename}-backports",
-      priority => 999
+  case $::lsbdistcodename {
+    'wheezy': {
+      # docker needs a newer kernel on wheezy
+      apt::preferences_snippet { ['linux-image-amd64']:
+        release  => "${::lsbdistcodename}-backports",
+        priority => 999
+      }
     }
+
+    'jessie': {
+      # install python-docker from backports on jessie
+      apt::preferences_snippet { ['python-docker']:
+        release  => "${::lsbdistcodename}-backports",
+        priority => 999
+      }
+    }
+
+    default:  { }
   }
 
   apt::preferences_snippet { ['python-tornado',

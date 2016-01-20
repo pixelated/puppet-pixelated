044e195bb42083ca2c5abb30591589f090cc3553
Vitor Barbosa <vitor.tfreitas@gmail.com>
2016-01-20 19:29:41 -0200

    Setting propper locale for leap deploy

--- vagrant_platform.sh
+++ vagrant_platform.sh
@@ -12,4 +12,4 @@ vagrant_ssh 'git submodule add https://github.com/pixelated/puppet-pixelated.git
 vagrant_ssh 'mkdir -p files/puppet/modules/custom/manifests'
 vagrant_ssh 'echo '{}' > services/pixelated.json'
 vagrant_ssh "echo 'class custom { include ::pixelated::dispatcher }' > files/puppet/modules/custom/manifests/init.pp"
-vagrant_ssh 'leap deploy'
+vagrant_ssh 'export LANG=en_US.UTF-8; export LANGUAGE=en_US.UTF-8; export LC_ALL=en_US.UTF-8; leap deploy'

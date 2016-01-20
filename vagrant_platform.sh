74d09134a4998626499d654b31923f46d2a19951
Christoph <ckluente@thoughtworks.com>
2016-01-20 14:25:01 +0100

    add script to build a complete platform inside vagrant.
    See https://github.com/pixelated/pixelated-platform/issues/153

---
+++ vagrant_platform.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function vagrant_ssh {
+  vagrant ssh -c "cd /home/vagrant/leap/configuration/; $1"
+}
+
+git clone --branch develop --recursive git@github.com:leapcode/leap_platform.git
+cd leap_platform
+vagrant up
+vagrant_ssh 'mkdir -p files/puppet/modules'
+vagrant_ssh 'git submodule add https://github.com/pixelated/puppet-pixelated.git files/puppet/modules/pixelated'
+vagrant_ssh 'mkdir -p files/puppet/modules/custom/manifests'
+vagrant_ssh 'echo '{}' > services/pixelated.json'
+vagrant_ssh "echo 'class custom { include ::pixelated::dispatcher }' > files/puppet/modules/custom/manifests/init.pp"
+vagrant_ssh 'leap deploy'

5f5bd9ffce4cc4f0ee52eb714e2c8debc261d803
Christoph <ckluente@thoughtworks.com>
2016-01-21 11:26:58 +0100

    set env variables for all commands

--- vagrant_platform.sh
+++ vagrant_platform.sh
@@ -2,7 +2,7 @@
 hash vagrant 2>/dev/null || { echo >&2 "Vagrant is not installed.  Aborting."; exit 1; }
 
 vagrant_ssh (){
-  vagrant ssh -c "cd /home/vagrant/leap/configuration/; $1"
+  vagrant ssh -c "export LANG=en_US.UTF-8; export LANGUAGE=en_US.UTF-8; export LC_ALL=en_US.UTF-8; cd /home/vagrant/leap/configuration/; $1"
 }
 
 #homebrew git does not work
@@ -14,4 +14,4 @@ vagrant_ssh 'git submodule add https://github.com/pixelated/puppet-pixelated.git
 vagrant_ssh 'mkdir -p files/puppet/modules/custom/manifests'
 vagrant_ssh 'echo '{}' > services/pixelated.json'
 vagrant_ssh "echo 'class custom { include ::pixelated::dispatcher }' > files/puppet/modules/custom/manifests/init.pp"
-vagrant_ssh 'export LANG=en_US.UTF-8; export LANGUAGE=en_US.UTF-8; export LC_ALL=en_US.UTF-8; leap deploy'
+vagrant_ssh 'leap deploy'

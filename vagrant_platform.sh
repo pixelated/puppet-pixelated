b54baec3fc8ca62f7765153bbbd3716f0d6ee43c
varac <varacanero@zeromail.org>
2016-01-26 09:31:04 +0100

    updated leap_platform if already checked out

--- vagrant_platform.sh
+++ vagrant_platform.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+#
+# Issues:
+# - homebrew version of git does not work nicely with submodules
+
 hash vagrant 2>/dev/null || { echo >&2 "Vagrant is not installed.  Aborting."; exit 1; }
 
 vagrant_ssh (){
   vagrant ssh -c "export LANG=en_US.UTF-8; export LANGUAGE=en_US.UTF-8; export LC_ALL=en_US.UTF-8; cd /home/vagrant/leap/configuration/; $1"
 }
 
-#homebrew git does not work
-/usr/bin/git clone --branch develop --recursive https://github.com/leapcode/leap_platform.git
-cd leap_platform
+if [ -d ./leap_platform ]
+then
+  cd leap_platform
+  /usr/bin/git pull
+  /usr/bin/git submodule update --init
+else
+  /usr/bin/git clone --branch develop --recursive https://github.com/leapcode/leap_platform.git
+  cd leap_platform
+fi
+
 vagrant up
 vagrant_ssh 'mkdir -p files/puppet/modules'
 vagrant_ssh 'git submodule add https://github.com/pixelated/puppet-pixelated.git files/puppet/modules/pixelated'

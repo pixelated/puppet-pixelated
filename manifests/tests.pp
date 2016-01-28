cf8322d9820bc8c64e54812ed3f6050b73202779
Christoph <ckluente@thoughtworks.com>
2016-01-28 10:12:37 +0100

    fix path to curl

--- manifests/tests.pp
+++ manifests/tests.pp
@@ -24,7 +24,7 @@ class pixelated::tests {
   }
 
   exec{'install_phantomjs':
-    command => ' curl -L https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 | tar xvj --strip-components=2 -C /usr/local/bin/ phantomjs-2.1.1-linux-x86_64/bin/phantomjs',
+    command => '/usr/bin/curl -L https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 | tar xvj --strip-components=2 -C /usr/local/bin/ phantomjs-2.1.1-linux-x86_64/bin/phantomjs',
     creates => '/usr/local/bin/phantomjs'
   }
 }

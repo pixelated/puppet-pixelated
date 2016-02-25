d39d1124a67f923616f2ae0bec35af5cdea804a4
varac <varacanero@zeromail.org>
2016-02-25 18:03:26 +0100

    installing phantomjs depends on bzip2

--- manifests/tests.pp
+++ manifests/tests.pp
@@ -19,7 +19,7 @@ class pixelated::tests {
     mode   => '0755',
   }
 
-  ensure_packages(['python-pip', 'curl'])
+  ensure_packages(['python-pip', 'curl', 'bzip2'])
 
   package{['behave','selenium']:
     ensure   => installed,
@@ -30,7 +30,7 @@ class pixelated::tests {
   exec{'install_phantomjs':
     command => '/usr/bin/curl -L https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 | tar xvj --strip-components=2 -C /usr/local/bin/ phantomjs-2.1.1-linux-x86_64/bin/phantomjs',
     creates => '/usr/local/bin/phantomjs',
-    require => Package['curl'],
+    require => [ Package['curl'], Package['bzip2'] ],
     notify  => Exec['check_phantomjs_sha'],
   }
   exec{'check_phantomjs_sha':

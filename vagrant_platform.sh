dd04f3f6378d18330d6ee6a201930d606fef1474
varac <varacanero@zeromail.org>
2016-03-07 12:41:45 +0100

    Simplify vagrant installation
    Resolves: https://github.com/pixelated/puppet-pixelated/issues/18

--- vagrant_platform.sh
+++ vagrant_platform.sh
@@ -5,10 +5,6 @@
 
 hash vagrant 2>/dev/null || { echo >&2 "Vagrant is not installed.  Aborting."; exit 1; }
 
-vagrant_ssh (){
-  vagrant ssh -c "export LANG=en_US.UTF-8; export LANGUAGE=en_US.UTF-8; export LC_ALL=en_US.UTF-8; cd /home/vagrant/leap/configuration/; $1"
-}
-
 if [ -d ./leap_platform ]
 then
   cd leap_platform
@@ -19,15 +15,5 @@ else
   cd leap_platform
 fi
 
-vagrant up --no-provision
+vagrant up pixelated --no-provision
 vagrant provision
-vagrant_ssh 'mkdir -p files/puppet/modules'
-vagrant_ssh 'git submodule add https://github.com/pixelated/puppet-pixelated.git files/puppet/modules/pixelated'
-vagrant_ssh 'mkdir -p files/puppet/modules/custom/manifests'
-vagrant_ssh 'echo '{}' > services/pixelated.json'
-vagrant_ssh "echo 'class custom { include ::pixelated}' > files/puppet/modules/custom/manifests/init.pp"
-vagrant_ssh 'leap deploy'
-
-# at last, check if everything is working
-vagrant_ssh 'leap test'
-

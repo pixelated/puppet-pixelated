5e1d676cd9dce1ed473bf921982593ae1703e7a8
varac <varacanero@zeromail.org>
2016-10-10 11:34:38 +0200

    Stop running tests at the first failure

--- manifests/tests.pp
+++ manifests/tests.pp
@@ -44,7 +44,7 @@ class pixelated::tests {
   }
 
   cron {'run_functional_tests':
-    command     => "(date; INVITE_CODE_ENABLED=${invite} /usr/bin/mk-job pixelated-functional-tests /usr/local/bin/behave --tags @staging --tags ~@wip --no-capture -k /srv/leap/tests_custom/functional-tests/) >> /var/log/check_mk_jobs.log 2>&1",
+    command     => "(date; INVITE_CODE_ENABLED=${invite} /usr/bin/mk-job pixelated-functional-tests /usr/local/bin/behave --stop --tags @staging --tags ~@wip --no-capture -k /srv/leap/tests_custom/functional-tests/) >> /var/log/check_mk_jobs.log 2>&1",
     environment => 'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     user        => 'root',
     minute      => 27,

66036a30071b5023cfea7c86408a379bc183fd9c
varac <varacanero@zeromail.org>
2016-10-10 12:01:19 +0200

    Fix puppet rspec test

--- spec/classes/tests_spec.rb
+++ spec/classes/tests_spec.rb
@@ -40,7 +40,7 @@ describe 'pixelated::tests' do
     end
     it do
       should contain_cron('run_functional_tests').with(
-        "command" => """(date; INVITE_CODE_ENABLED=true /usr/bin/mk-job pixelated-functional-tests /usr/local/bin/behave --tags @staging --tags ~@wip --no-capture -k /srv/leap/tests_custom/functional-tests/) >> /var/log/check_mk_jobs.log 2>&1"
+        "command" => """(date; INVITE_CODE_ENABLED=true /usr/bin/mk-job pixelated-functional-tests /usr/local/bin/behave --stop --tags @staging --tags ~@wip --no-capture -k /srv/leap/tests_custom/functional-tests/) >> /var/log/check_mk_jobs.log 2>&1"
       )
     end
   end

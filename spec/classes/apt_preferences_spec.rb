0b29fe628ee009489a74d185c500a7ec5c5aaa6b
Anike Arni <aarni@thoughtworks.com>
2017-02-08 12:32:38 -0200

    Fix apt-preferences test pinning

--- spec/classes/apt_preferences_spec.rb
+++ spec/classes/apt_preferences_spec.rb
@@ -17,7 +17,7 @@ describe 'pixelated::apt::preferences' do
 
   describe 'pixelated packages' do
     it { should contain_apt__preferences_snippet("pixelated").with_pin('origin "packages.pixelated-project.org"')}
-    it { should contain_apt__preferences_snippet("pixelated").with_priority('1001')}
+    it { should contain_apt__preferences_snippet("pixelated").with_priority('1000')}
   end
 
   %w( soledad-server soledad-client soledad-common leap-keymanager leap-auth).each do | file |
